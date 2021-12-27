659f6a87b1a4652800e188366aa4cba7ef0e65e4
lLeahDizon <516330797@qq.com>
2021-12-27 20:37:10 +0800

    改写成.ts

--- app2.ts
+++ app2.ts
@@ -1,14 +1,45 @@
-import express from 'express';
+import {ErrorRequestHandler, RequestHandler} from 'express';
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
 
 const app = express();
-const port = 4000;
 
-app.get('/', (request, response) => {
-  response.send('你好');
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.post('/xxx', (request, response, next) => {});
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+console.log('this is app2.ts');
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+} as RequestHandler);
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+  next();
+} as ErrorRequestHandler);
+
+module.exports = app;
--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+import {RequestHandler} from 'express';
+
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+} as RequestHandler);
 
 module.exports = router;
